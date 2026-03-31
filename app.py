69a3bde0ad7b83ea35f6bdc0a12072d9489d0062
kuzo96 <kuzo9601@gmail.com>
2026-03-31 04:36:17 +0000

    updatev4

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello DevOps v2.1"
+    return "Hello DevOps v3.0"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
