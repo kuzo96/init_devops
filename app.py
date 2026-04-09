71eeedfa722214a8b522beacf90deae47d849ab7
kuzo96 <kuzo9601@gmail.com>
2026-04-09 11:13:04 +0700

    commitv5

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from prometheus_client import Counter, generate_latest
+from prometheus_client import CONTENT_TYPE_LATEST
+
 app = Flask(__name__)
+
+REQUEST_COUNT = Counter('app_requests_total', 'Total number of requests')
 
 @app.route('/')
 def hello():
-    return "Hello DevOps v4.6"
+    REQUEST_COUNT.inc()
+    return "Hello DevOps v5 🚀"
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)  
