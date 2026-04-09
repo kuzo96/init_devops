25f9819a82aa13692ee3240a5bff3e3f6c60a4f2
kuzo96 <kuzo9601@gmail.com>
2026-04-09 10:58:14 +0700

    commitv4.5

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello DevOps v4.4"
+    return "Hello DevOps v4.5"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
